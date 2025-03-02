177893c02e7047e67f4a9c6f4f2753443aab20a6
unknown <sakuragungde@gmail.com>
2025-03-02 18:03:47 +0700

    tes_1

--- tugas_1.cpp
+++ tugas_1.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 class MHS {
   public:
-  string MhsName;
+  string MHSName;
 
   void printName() {
-    cout << "Nama Mahasiswa: " << MhsName;
+    cout << "MHS name is : " << MhsName;
   }
 };
 
 int main() {
   MHS mhs1;
-  mhs1.MhsName = "afin pujo ardi";
+  mhs1.MHSName = "afin pujo ardi";
   mhs1.printName();
   return 0;
 } 
